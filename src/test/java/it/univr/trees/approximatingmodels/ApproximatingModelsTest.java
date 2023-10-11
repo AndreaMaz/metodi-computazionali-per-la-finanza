1364e3bb7eca18527570683452b4f702f5e24491
Andrea Mazzon <mazzon@math.lmu.de>
2023-10-11 19:37:41 +0200

    aggiunti test (parziale)

--- src/test/java/it/univr/trees/approximatingmodels/ApproximatingModelsTest.java
+++ src/test/java/it/univr/trees/approximatingmodels/ApproximatingModelsTest.java
@@ -23,54 +23,6 @@ public class ApproximatingModelsTest {
 		
 		double strike = 100;
 
-		JarrowRuddModel ourModel = new JarrowRuddModel(spotPrice, riskFreeRate, volatility, lastTime, numberOfTimes);		
-		
-		DoubleUnaryOperator payoffFunction = (x) -> (x - strike > 0 ? x - strike : 0.0);
-		double[] indicatorsValuesBiggerEqualInitialValuesAtFinalTime = ourModel.getTransformedValuesAtGivenTime(lastTime,payoffFunction);
-		double[] probabilitiesValuestFinalTime = ourModel.getValuesProbabilitiesAtGivenTime(lastTime);
-
-		
-		double probabilityBiggerEqualInitialValue = UsefulMethodsForArrays.getScalarProductTwoArrays(indicatorsValuesBiggerEqualInitialValuesAtFinalTime, probabilitiesValuestFinalTime);
-		System.out.println(Math.exp(-riskFreeRate*lastTime)*probabilityBiggerEqualInitialValue);
-
-		double bsPrice = AnalyticFormulas.blackScholesOptionValue(spotPrice,riskFreeRate,volatility, lastTime,strike,true);
-		System.out.println(bsPrice);
-		
-		EuropeanNonPathDependentOption ourOption = new EuropeanNonPathDependentOption(lastTime, payoffFunction);
-		
-		System.out.println(ourOption.getValue(ourModel));
-		
-		
-		
-		DoubleUnaryOperator dummyFunctionBlackScholesPrice = (numberOfTimesForFunction) -> {
-			//return AnalyticFormulas.blackScholesDigitalOptionValue(spotPrice, riskFreeRate, volatility, lastTime, strike);
-			return AnalyticFormulas.blackScholesOptionValue(spotPrice,riskFreeRate,volatility, lastTime,strike,true);
-		};
-		
-		DoubleUnaryOperator numberOfTimesToPriceCoxRossRubinsteinModel = (numberOfTimesForFunction) -> {
-			CoxRossRubinsteinModel ourModelForFunction = new CoxRossRubinsteinModel(spotPrice, riskFreeRate, volatility, lastTime, (int) numberOfTimesForFunction);		
-			return ourOption.getValue(ourModelForFunction);
-		};
-		
-		DoubleUnaryOperator numberOfTimesToPriceJarrowRuddModel = (numberOfTimesForFunction) -> {
-			JarrowRuddModel ourModelForFunction = new JarrowRuddModel(spotPrice, riskFreeRate, volatility, lastTime, (int) numberOfTimesForFunction);		
-			return ourOption.getValue(ourModelForFunction);
-		};
-		
-		
-		DoubleUnaryOperator numberOfTimesToPriceLeisenReimer = (numberOfTimesForFunction) -> {
-			LeisenReimerModel ourModelForFunction = new LeisenReimerModel(spotPrice, riskFreeRate, volatility, lastTime, (int) numberOfTimesForFunction, strike);		
-			return ourOption.getValue(ourModelForFunction);
-		};
-		
-		
-		final Plot plot = new Plot2D(50, 4000.0, 20, Arrays.asList(
-				new Named<DoubleUnaryOperator>("Black Scholes Price", dummyFunctionBlackScholesPrice),
-				new Named<DoubleUnaryOperator>("Cox Ross Rubinstein", numberOfTimesToPriceCoxRossRubinsteinModel),
-				new Named<DoubleUnaryOperator>("Jarrow Rudd", numberOfTimesToPriceJarrowRuddModel),
-				new Named<DoubleUnaryOperator>("Leisen Reimer", numberOfTimesToPriceLeisenReimer)));
-		plot.setIsLegendVisible(true);
-		plot.show();
 		
 
 		
