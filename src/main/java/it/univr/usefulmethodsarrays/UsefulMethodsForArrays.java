e1e43f2381563e780f735740a95780dc8d223f53
Andrea Mazzon <mazzon@math.lmu.de>
2023-11-06 15:57:30 +0100

    Aggiunto metodo per la media degli elementu di un array

--- src/main/java/it/univr/usefulmethodsarrays/UsefulMethodsForArrays.java
+++ src/main/java/it/univr/usefulmethodsarrays/UsefulMethodsForArrays.java
@@ -3,8 +3,8 @@ package it.univr.usefulmethodsarrays;
 import java.util.function.DoubleUnaryOperator;
 
 public class UsefulMethodsForArrays {
-	
-	
+
+
 	/**
 	 * It returns the sum of the elements of an array
 	 * 
@@ -18,116 +18,130 @@ public class UsefulMethodsForArrays {
 		}
 		return sum;
 	}
+
 	
-		/**
-		 * It returns the product of two arrays
-		 *
-		 * @param firstArray
-		 * @param secondArray
-		 * @return an array representing the element-wise product of the two arrays
-		 */
-		public static double[] multArrays(double[] firstArray, double[] secondArray) {
-			int firstLength = firstArray.length;
-			if (firstLength != secondArray.length) {
-				throw new IllegalArgumentException("Error: the two arrays must have same length!");
-			}
-			double[] product = new double[firstLength];
-			for (int i = 0; i < firstLength; i++) {
-				product[i] = firstArray[i] * secondArray[i];
-			}
-			return product;
+	/**
+	 * It returns the average of the elements of an array
+	 * 
+	 * @param array
+	 * @return the sum of the elements of the array
+	 */
+	public static double getAverage(double array[]) {
+		double sum = 0.0;
+		for(double value : array) {
+			sum += value;
+		}
+		return sum/array.length;
+	}
+	/**
+	 * It returns the product of two arrays
+	 *
+	 * @param firstArray
+	 * @param secondArray
+	 * @return an array representing the element-wise product of the two arrays
+	 */
+	public static double[] multArrays(double[] firstArray, double[] secondArray) {
+		int firstLength = firstArray.length;
+		if (firstLength != secondArray.length) {
+			throw new IllegalArgumentException("Error: the two arrays must have same length!");
+		}
+		double[] product = new double[firstLength];
+		for (int i = 0; i < firstLength; i++) {
+			product[i] = firstArray[i] * secondArray[i];
+		}
+		return product;
+	}
+
+	/**
+	 * It returns the sum of two arrays
+	 *
+	 * @param firstArray
+	 * @param secondArray
+	 * @return an array representing the element-wise sum of the two arrays
+	 */
+	public static double[] sumArrays(double[] firstArray, double[] secondArray) {
+		int firstLength = firstArray.length;
+		if (firstLength != secondArray.length) {
+			throw new IllegalArgumentException("Error: the two arrays must have same length!");
 		}
 
-		/**
-		 * It returns the sum of two arrays
-		 *
-		 * @param firstArray
-		 * @param secondArray
-		 * @return an array representing the element-wise sum of the two arrays
-		 */
-		public static double[] sumArrays(double[] firstArray, double[] secondArray) {
-			int firstLength = firstArray.length;
-			if (firstLength != secondArray.length) {
-				throw new IllegalArgumentException("Error: the two arrays must have same length!");
-			}
-
-			double[] difference = new double[firstLength];
-			for (int i = 0; i < firstLength; i++) {
-				difference[i] = firstArray[i] + secondArray[i];
-			}
-			return difference;
+		double[] difference = new double[firstLength];
+		for (int i = 0; i < firstLength; i++) {
+			difference[i] = firstArray[i] + secondArray[i];
 		}
-		
-		/**
-		 * It returns the difference of two arrays
-		 *
-		 * @param firstArray
-		 * @param secondArray
-		 * @return an array representing the element-wise difference of the two arrays
-		 */
-		public static double[] diffArrays(double[] firstArray, double[] secondArray) {
-			int firstLength = firstArray.length;
-			if (firstLength != secondArray.length) {
-				throw new IllegalArgumentException("Error: the two arrays must have same length!");
-			}
+		return difference;
+	}
 
-			double[] difference = new double[firstLength];
-			for (int i = 0; i < firstLength; i++) {
-				difference[i] = firstArray[i] - secondArray[i];
-			}
-			return difference;
+	/**
+	 * It returns the difference of two arrays
+	 *
+	 * @param firstArray
+	 * @param secondArray
+	 * @return an array representing the element-wise difference of the two arrays
+	 */
+	public static double[] diffArrays(double[] firstArray, double[] secondArray) {
+		int firstLength = firstArray.length;
+		if (firstLength != secondArray.length) {
+			throw new IllegalArgumentException("Error: the two arrays must have same length!");
 		}
 
-		/**
-		 * It returns the element-wise ratio of two arrays
-		 *
-		 * @param firstArray
-		 * @param secondArray
-		 * @return an array representing the element-wise ratio of the two arrays
-		 */
-		public static double[] ratioArrays(double[] firstArray, double[] secondArray) {
-			int firstLength = firstArray.length;
-			if (firstLength != secondArray.length) {
-				throw new IllegalArgumentException("Error: the two arrays must have same length!");
-			}
-			double[] ratio = new double[firstLength];
-			for (int i = 0; i < firstLength; i++) {
-				ratio[i] = firstArray[i] / secondArray[i];
-			}
-			return ratio;
+		double[] difference = new double[firstLength];
+		for (int i = 0; i < firstLength; i++) {
+			difference[i] = firstArray[i] - secondArray[i];
 		}
+		return difference;
+	}
 
-		/**
-		 * It returns an array transformedArray where transformedArray[i] = f(originalArray[i]), with
-		 * f given in input as a DoubleUnaryOperator.
-		 *  
-		 * @param originalArray, 
-		 * @param transformFunction, the funtion f above
-		 * @return  an array transformedArray where transformedArray[i] = f(originalArray[i])
-		 */
-		public static double[] applyFunctionToArray(double[] originalArray, DoubleUnaryOperator transformFunction) {
-			int lengthOriginalArray = originalArray.length;
-			double[] transformedArray = new double[lengthOriginalArray];
-			for (int i = 0; i<lengthOriginalArray; i++) {
-				//this is how DoubleUnaryOperator work: applyAsDouble is the method of the interface they implement
-				//transformedArray[i] = f(originalArray[i) where f is the function represented by transformFunction
-				transformedArray[i] = transformFunction.applyAsDouble(originalArray[i]);
-			}
-			return transformedArray;
+	/**
+	 * It returns the element-wise ratio of two arrays
+	 *
+	 * @param firstArray
+	 * @param secondArray
+	 * @return an array representing the element-wise ratio of the two arrays
+	 */
+	public static double[] ratioArrays(double[] firstArray, double[] secondArray) {
+		int firstLength = firstArray.length;
+		if (firstLength != secondArray.length) {
+			throw new IllegalArgumentException("Error: the two arrays must have same length!");
 		}
-		
-		
-		/**
-		 * It returns the scalar product of two arrays
-		 *
-		 * @param firstArray
-		 * @param secondArray
-		 * @return an array representing the element-wise product of the two arrays
-		 */
-		public static double getScalarProductTwoArrays(double[] firstArray, double[] secondArray) {
-			double[] productOfArrays = multArrays(firstArray, secondArray);
-			return getSum(productOfArrays);
+		double[] ratio = new double[firstLength];
+		for (int i = 0; i < firstLength; i++) {
+			ratio[i] = firstArray[i] / secondArray[i];
 		}
-		
-		
+		return ratio;
+	}
+
+	/**
+	 * It returns an array transformedArray where transformedArray[i] = f(originalArray[i]), with
+	 * f given in input as a DoubleUnaryOperator.
+	 *  
+	 * @param originalArray, 
+	 * @param transformFunction, the funtion f above
+	 * @return  an array transformedArray where transformedArray[i] = f(originalArray[i])
+	 */
+	public static double[] applyFunctionToArray(double[] originalArray, DoubleUnaryOperator transformFunction) {
+		int lengthOriginalArray = originalArray.length;
+		double[] transformedArray = new double[lengthOriginalArray];
+		for (int i = 0; i<lengthOriginalArray; i++) {
+			//this is how DoubleUnaryOperator work: applyAsDouble is the method of the interface they implement
+			//transformedArray[i] = f(originalArray[i) where f is the function represented by transformFunction
+			transformedArray[i] = transformFunction.applyAsDouble(originalArray[i]);
+		}
+		return transformedArray;
+	}
+
+
+	/**
+	 * It returns the scalar product of two arrays
+	 *
+	 * @param firstArray
+	 * @param secondArray
+	 * @return an array representing the element-wise product of the two arrays
+	 */
+	public static double getScalarProductTwoArrays(double[] firstArray, double[] secondArray) {
+		double[] productOfArrays = multArrays(firstArray, secondArray);
+		return getSum(productOfArrays);
+	}
+
+
 }
