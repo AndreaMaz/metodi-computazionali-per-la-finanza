daabc838ace909be706fef9f14e99cdc81da68da
Andrea Mazzon <mazzon@math.lmu.de>
2023-11-22 18:48:18 +0100

    Inizio costruzione AssetModelMonteCarloSimulationModel

--- src/test/java/it/univr/montecarlo/discretizationschemes/finmathlibraryimplementation/FinmathLibraryTest.java
+++ src/test/java/it/univr/montecarlo/discretizationschemes/finmathlibraryimplementation/FinmathLibraryTest.java
@@ -2,6 +2,11 @@ package it.univr.montecarlo.discretizationschemes.finmathlibraryimplementation;
 
 import net.finmath.exception.CalculationException;
 import net.finmath.functions.AnalyticFormulas;
+import net.finmath.montecarlo.IndependentIncrements;
+import net.finmath.montecarlo.assetderivativevaluation.AssetModelMonteCarloSimulationModel;
+import net.finmath.montecarlo.assetderivativevaluation.MonteCarloAssetModel;
+import net.finmath.montecarlo.assetderivativevaluation.products.EuropeanOption;
+import net.finmath.montecarlo.model.ProcessModel;
 
 
 /**
@@ -16,30 +21,53 @@ public class FinmathLibraryTest {
 
 	public static void main(String[] args) throws CalculationException {
 		
-		//paremeters for the option
+		//parameters for the option
+
 		double maturity = 1.0;
+
 		double strike = 2.0;
-		
+
 		//parameters for the model (i.e., for the SDE)
+
 		double initialValue = 2.0;
+
 		double riskFreeRate = 0.0;
+
 		double volatility = 0.2;
-	
+
 		//we compute and print the analytic value
-		double analyticValue = AnalyticFormulas.bachelierOptionValue(initialValue, volatility, maturity, strike, 1);
+
+		double analyticValue = AnalyticFormulas.bachelierOptionValue(initialValue, volatility, maturity, strike, 1.0);
+
 		System.out.println("The analytic value of the option is " + analyticValue);
-		
+
 		//parameters for the discretization scheme and the Monte Carlo simulation
+
 		double timeStep = 0.05;
+
 		int numberOfTimesSteps = (int) (maturity/timeStep);
+
 		int numberOfSimulatedPaths = 10000;
-		
+
 		int seed = 1897;
-		
-	
-		
+
+		EuropeanOption ourOption = new EuropeanOption(maturity, strike);
+
+		ProcessModel model = null;
+
+		IndependentIncrements stochasticDriver = null;
+
+		AssetModelMonteCarloSimulationModel ourSimulation = new MonteCarloAssetModel(model, stochasticDriver);//how to construct it?
+
 		//we print and compute the Monte Carlo value
+
 		double monteCarloValue = 0.0; //we have to change this value
-		System.out.println("The Monte carlo value of the option is " + monteCarloValue);		
+
+		System.out.println("The Monte carlo value of the option is " + monteCarloValue);
+
+
+
+
+		
 	}
 }
